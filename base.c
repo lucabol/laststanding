0d5ab8cd0df15e5a2e86d99ba6904620e079105c
Luca Bolognese <lucabol@microsoft.com>
2021-11-10 14:10:34 +0100

    Fix bug in lasts.h

--- base.c
+++ base.c
@@ -4,8 +4,11 @@ int argc_;
 char** argv_;
 
 int args() {
+    // Test that at least I got the program name.
     test_cond(argc_ > 0);
+    test_cond(strlen(argv_[0]) > 0);
 
+    // Print all parameters (i.e., test for unicode).
     for(int i = 0; i < argc_; i++) {
         write(STDOUT, argv_[i], strlen(argv_[i]));
         write(STDOUT, "\n", 1);
@@ -15,6 +18,7 @@ int args() {
 int main(int argc, char* argv[]) {
     argc_ = argc;
     argv_ = argv;
+
     test_run(args);
     return 0;
 }
