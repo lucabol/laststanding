74e132e8ef0b6e129adcef36e752748f22808072
Luca Bolognese <lucabol@microsoft.com>
2023-12-13 16:09:50 +0100

    Changed to attribute used

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 REM Works in x64 Native Tools Command Prompt
-clang -I. test\countlines.c -O3 -lkernel32 -ffreestanding -o countlines.exe
-clang -I. test\test.c -O3 -lkernel32 -ffreestanding -o test.exe
+clang -I. test\countlines.c -O3 -lkernel32 -ffreestanding -o bin\countlines.exe
+clang -I. test\test.c -O3 -lkernel32 -ffreestanding -o bin\test.exe
