bbc716ca598836f30a133a6a317f198c8c07db0a
Luca Bolognese <lucabol@microsoft.com>
2023-12-13 16:08:51 +0100

    Changed to attribute used

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-# Works in x64 Native Tools Command Prompt
+REM Works in x64 Native Tools Command Prompt
 clang -I. test\countlines.c -O3 -lkernel32 -ffreestanding -o countlines.exe
 clang -I. test\test.c -O3 -lkernel32 -ffreestanding -o test.exe
